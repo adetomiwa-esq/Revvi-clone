894cbf2c12cdb48f97c2155840a324b5f0ce2942
Akinola Kehinde <khennyakin@gmail.com>
2023-06-04 22:50:02 +0100

    added date of birth options to payprogram page

--- src/Components/PayProgram/TopSection.tsx
+++ src/Components/PayProgram/TopSection.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Months, Days, Years } from '../Utility/Date';
 
 function TopSection() {
 
@@ -10,12 +11,15 @@ function TopSection() {
             <div className='flex mb-2'>
                 <select name="" id="" className='w-[130px] h-9 outline-none px-[2px] mr-5 bg-zinc-50 border border-zinc-300'>
                     <option value="">Month</option>
+                    {Months.map((month, index) => <option key={index} value={month}>{month}</option>)}
                 </select>
                 <select name="" id="" className='w-[130px] h-9 mr-5 outline-none px-[2px] bg-zinc-50 border border-zinc-300'>
                     <option value="">Day</option>
+                    {Days.map((day, index) => <option key={index} value={day}>{day}</option>)}
                 </select>
                 <select name="" id="" className='w-[130px] h-9 outline-none px-[2px] bg-zinc-50 border border-zinc-300'>
                     <option value="">Year</option>
+                    {Years.map((year, index) => <option key={index} value={year}>{year}</option>)}
                 </select>
             </div>
           </div>
